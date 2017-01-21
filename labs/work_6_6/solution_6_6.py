0bcbe887ef1ad38e79dcfe4dd22a926fb2270273
Mailtask <marryjones789@gmail.com>
2017-01-21 12:44:27 +0200

    laboratory work 6.6 solution README.md added

--- labs/work_6_6/solution_6_6.py
+++ labs/work_6_6/solution_6_6.py
@@ -8,9 +8,10 @@ print(time.ctime())  # print current time in format 'Tue May 24 14:09:17 2016’
 print(time.localtime().tm_year)  # Current time year
 print(time.localtime().tm_yday)  # Current year day
 
-print (time.strftime("%d %m. %Y %H:M",time.gmtime()))
-print (datetime.datetime.strptime("19 Sep. 2012 10:15", "%d %b. %Y %H:%M"))
-# time_1 = datetime.datetime.date(datetime.datetime.now().today() - 1)   # Create datetime tuple with current day minus one day
-# datetime.timedelta(datetime.datetime.now() - time_1)  # Check the difference with datime delta
+print(time.strftime("%d %m. %Y %H:M",time.gmtime()))
+print(datetime.datetime.strptime("19 Sep. 2012 10:15", "%d %b. %Y %H:%M"))
+time_1 = datetime.datetime.now() + datetime.timedelta(days = -1)   # Create datetime tuple with current day minus one day
+now = datetime.datetime.now()
+print('Time delta', (now - time_1).days)  # Check the difference with time delta
 
-print("Script execution time:%f4.2"%time.clock())
+print("Script execution time: %f4.2" % time.clock())
